99a043eec890d10dc16430d2ad79897adec6cc74
Dmitry Lapynov <dmitry.lapynov@corp.mail.ru>
2015-05-31 15:48:13 +0300

    Patrickh Lauke presentation

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
+# Доклады
+
+## Избранные
+[Getting touchy by Patrickh Lauke](https://patrickhlauke.github.io/getting-touchy-presentation/)
+
+## Мои
+[Мобильный веб](/pres/mobiles/?full#cover)
+
 ## Обзоры конференций
 [FrontendConf 2015, Москва](/overview/frontendconfmsk2015)
-
-## Доклады
-[Мобильный веб](/pres/mobiles/?full#cover)
--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-markdown-html -w -t 'leikam.github.io' -s './css/md.css' -w ./overview/frontendconfmsk2015/README.md -o ./overview/frontendconfmsk2015/index.html
+markdown-html -w -t 'FrontendConf 2015, Moscow' -s './css/md.css' -w ./review/frontendconfmsk2015/README.md -o ./review/frontendconfmsk2015/index.html
+
+markdown-html -w -t 'leikam.github.io' -s './css/md.css' -w ./README.md -o ./index.html
