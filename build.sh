0b96f1e1236fc23c395bf9766f92d7bb2e979473
Dmitry Lapynov <dmitry.lapynov@corp.mail.ru>
2015-06-04 18:25:33 +0300

    device pixel ratio test; +build.sh;

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-markdown-html -w -t 'FrontendConf 2015, Moscow' -s './css/md.css' -w ./review/frontendconfmsk2015/README.md -o ./review/frontendconfmsk2015/index.html
-
-markdown-html -w -t 'leikam.github.io' -s './css/md.css' -w ./README.md -o ./index.html
+PAGES="Home FrontConf"
+select k in $PAGES;
+    do
+        if [ "$k" = "Home" ]; then
+            echo $k.md is watched..
+            markdown-html -w -t 'FrontendConf 2015, Moscow' -s './css/md.css' -w ./review/frontendconfmsk2015/README.md -o ./review/frontendconfmsk2015/index.html
+            exit
+        elif [ "$k" = "FrontConf" ]; then
+            echo $k.md is watched..
+            markdown-html -w -t 'leikam.github.io' -s './css/md.css' -w ./README.md -o ./index.html
+            exit;
+        else
+            echo bad choice.
+            exit
+        fi
+    done
